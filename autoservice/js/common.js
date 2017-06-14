b47bcf2deee795fbe2ec5e206b4d36197ce628d5
Alex Mihailus <mihailus.a.g@gmail.com>
2017-06-14 11:33:23 +0300

    autoservice 1

--- autoservice/js/common.js
+++ autoservice/js/common.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+	
+	$(".mc_item_wrap ul").each(function(){
+		$(this).after("<div class='mc_item_wrap_af'</div>");
+	});
+	$(".mc_toggle").click(function(){
+		if($(this).parent().parent().children("ul").is(":visible")) {
+			$(this).parent().parent().children("ul").slideUp();
+			$(this).parent().parent().children(".mc_item_wrap_af").hide();
+		} else{
+	$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
+	$(".mc_item_wrap").removeClass("active");
+	$(this).parent().parent().addClass("active");
+	$(this).parent().parent().children("ul").slideDown();
+	$(this).parent().parent().children('.mc_item_wrap_af').show(0);
+	}
+
+
+	});
 
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
@@ -18,7 +36,7 @@ $(document).ready(function() {
 	//equalheight - одинаковая высота колонок
 	//Пример списка элементов:
 	//var eqElement = ".cat_container > div, .home_news > div"
-	var eqElement = ".element"
+	var eqElement = ".hi_item, .sb_content > div"
 	$(window).load(function(){equalheight(eqElement);}).resize(function(){equalheight(eqElement);});
 
 	//Masked Input Plugin
