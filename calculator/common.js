13edf6bd351357963cb3786ce6c66d40ff6b6dc9
Alex Mihailus <mihailus.a.g@gmail.com>
2017-06-19 10:21:43 +0300

    calculator

---
+++ calculator/common.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    var Button = $(".cal_button")
+    var Display = $(".cal_display");
+    var Equally = $(".cal_equally");
+    var Clear = $(".cal_clear");
+    var Sqrt = $(".cal_sqrt");
+    var Backspace = $(".backspace");
+
+   
+    Button.on('click', function () {
+        if (Display.val().length < 16) {
+        Display.val(Display.val() + $(this).attr('value') );
+        }
+    });
+
+    Clear.on('click', function () {
+        Display.val('');
+    });
+
+    Equally.on('click', function () {
+        Display.val( eval( Display.val() ) );
+    });
+
+     Sqrt.on('click', function () {
+        Display.val( Math.sqrt( Display.val() ) );
+    });
+
+    Backspace.on('click', function () {
+        Display.val( Display.val().substring(0, Display.val().length-1) );
+    });
+
+
+});
